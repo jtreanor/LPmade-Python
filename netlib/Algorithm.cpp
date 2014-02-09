c214d14091ce895d703671291dbf11aa47d62430
James Treanor <jct1@student.cs.ucc.ie>
2014-02-09 19:54:56 +0000

    syntax

--- netlib/Algorithm.cpp
+++ netlib/Algorithm.cpp
@@ -22,7 +22,7 @@ Algorithm::~Algorithm() {
 const WeightedNetwork& Algorithm::networkForDirection(int direction) const {
 	switch (direction) {
 		case UNDIRECTED:
-			std::cout << this->undirectedNetwork.edgeCount() < "\n";
+			std::cout << this->undirectedNetwork.edgeCount() << "\n";
 			return this->undirectedNetwork;
 		case REVERSED:
 			return this->reversedNetwork;
