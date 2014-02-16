cf929cf4d20db45e24e97cfbafd6d49e018810a3
James Treanor <jct1@student.cs.ucc.ie>
2014-02-16 16:17:58 +0000

    Alternative Salsa implementation

--- netlib/LinkPredictor/WTFLinkPredictor.cpp
+++ netlib/LinkPredictor/WTFLinkPredictor.cpp
@@ -63,24 +63,38 @@ double WTFLinkPredictor::generateScore( unsigned int vertex, unsigned int neighb
         this->salsaNetwork = this->network.salsaNetwork( this->hubs );
 
         // Start at random hub
-        vertex_t currentVertex = this->randomHub();
-        this->scores.at( currentVertex )++;
+        // vertex_t currentVertex = this->randomHub();
+        // this->scores.at( currentVertex )++;
 
         for ( unsigned int step = 1; true; step++ )
         {
-            //Go to auth
-            currentVertex = this->nextVertex( currentVertex, true );
-            this->scores.at( currentVertex ) += 2;
+            for(vertex_t hub: this-> hubs) {
+                double nbSum = 0.0;
+                const neighbor_set_t &neighbors = this->salsaNetwork.outNeighbors( hub );
+                for(neighbor_t auth_neighbor : neighbors) {
+                    nbSum += this->scores.at(auth_neighbor.first) / this->salsaNetwork.outDegree( auth_neighbor.first );
+                }
+                    
+                this->scores.at(hub) = nbSum;
+            }
 
-            //Go to hub
-            currentVertex = this->nextVertex( currentVertex, false );
-            this->scores.at( currentVertex )++;
+            for(vertex_t auth: this->authorities) {
+                this->scores.at(auth) = 0;
+            }
 
-            if ( step == 100000 )
+            for(vertex_t hub: this-> hubs) {
+                double myContribution = this->scores.at(hub) / this->salsaNetwork.outDegree(hub);
+                const neighbor_set_t &neighbors = this->salsaNetwork.outNeighbors( hub );
+                for(neighbor_t auth_neighbor : neighbors) {
+                    this->scores.at(auth_neighbor.first) += myContribution;
+                }
+            }
+
+            if ( step == 1 )
             {
                 oldScores = this->scores;
             }
-            else if ( step % 100000 == 0 )
+            else if ( step % 2 == 0 )
             {
                 double r = Statistics<double>::sampleCorrelationCoefficient( oldScores, this->scores );
                 if ( r > 0.9999 )
