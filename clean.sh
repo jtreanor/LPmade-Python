b7996156e449d6c1384401fbe92c022105fd3215
James Treanor <jtreanor3@gmail.com>
2014-04-11 23:05:45 +0100

    Removed clean script

--- clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf build/
-rm -f LPmade.py
-rm -f LPmade_wrap.cpp
-rm -f /usr/local/lib/python3.3/site-packages/LPmade*
-rm -f /usr/local/lib/python3.3/site-packages/_LPmade*
-rm -f /Library/Python/2.7/site-packages/LPmade*
-rm -f /Library/Python/2.7/site-packages/_LPmade*
